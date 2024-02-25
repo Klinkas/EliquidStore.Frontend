3a406904a18f78761c647ceaa88a85de55ba93af
Klinkas <versike727@gmail.com>
2024-02-25 20:54:05 +0500

    Added 2 methods `Delete` and `Open` for Eliquids component

--- eliquidstore/app/components/Eliquids.tsx
+++ eliquidstore/app/components/Eliquids.tsx
@@ -3,19 +3,24 @@ import { CardName } from "./Cardname"
 import Button from "antd/es/button/button"
 
 interface Props {
-    eliquids: Eliquid[]
+    eliquids: Eliquid[];
+    handleDelete: (id: string) => void;
+    handleOpen: (eliquid: Eliquid) => void;
 }
 
-export const Eliquids = ({eliquids}: Props) => {
+export const Eliquids = ({eliquids, handleDelete, handleOpen}: Props) => {
     return (
         <div className="cards">
             {eliquids.map((eliquid : Eliquid) => (
-                <Card key={eliquid.id} title={<CardName name={eliquid.name} flavor={eliquid.flavor} capacity={eliquid.capacity}/>} bordered={false}/>
+                <Card key={eliquid.id} title={<CardName name={eliquid.name} flavor={eliquid.flavor} capacity={eliquid.capacity}/>} bordered={false}>
+
+                    <div className="card__buttons">
+                        <Button onClick={() => handleOpen(eliquid)} style={{flex: 1}}>Изменить</Button>
+                        <Button onClick={() => handleDelete(eliquid.id)} danger style={{flex: 1}}>Удалить</Button>
+                    </div>
+                </Card>
+                
             ))}
-            <div className="card__buttons">
-                <Button>Изменить</Button>
-                <Button>Удалить</Button>
-            </div>
         </div>
     )
 }
